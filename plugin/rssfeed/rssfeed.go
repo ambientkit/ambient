8f471c1a12e2b77588c3a9fc22d26761dea146fb
Joseph Spurrier <github@josephspurrier.com>
2021-03-30 05:23:08 -0400

    Remove sitemap and rss from the dashboard page

--- plugin/rssfeed/rssfeed.go
+++ plugin/rssfeed/rssfeed.go
@@ -34,7 +34,46 @@ func (p *Plugin) Enable(toolkit *core.Toolkit) error {
 	return nil
 }
 
+const (
+	rssURL = "/rss.xml"
+)
+
+// Assets returns a list of assets and an embedded filesystem.
+func (p *Plugin) Assets() ([]core.Asset, *embed.FS) {
+	siteTitle, err := p.Site.Title()
+	if err != nil {
+		return nil, nil
+	}
+
+	return []core.Asset{
+		{
+			Filetype:   core.AssetGeneric,
+			Location:   core.LocationHead,
+			TagName:    "link",
+			ClosingTag: false,
+			Attributes: []core.Attribute{
+				{
+					Name:  "rel",
+					Value: "alternative",
+				},
+				{
+					Name:  "href",
+					Value: rssURL,
+				},
+				{
+					Name:  "application",
+					Value: "rss+xml",
+				},
+				{
+					Name:  "title",
+					Value: siteTitle,
+				},
+			},
+		},
+	}, &assets
+}
+
 // Routes gets routes for the plugin.
 func (p *Plugin) Routes() {
-	p.Router.Get("/rss.xml", p.index)
+	p.Router.Get(rssURL, p.index)
 }
