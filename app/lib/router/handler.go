c99f0511cb246d415c53469b2d2f7d23a8e58ed6
Joseph Spurrier <github@josephspurrier.com>
2021-04-04 07:14:43 -0400

    Don't expose router functions that aren't used

--- app/lib/router/handler.go
+++ app/lib/router/handler.go
@@ -5,12 +5,12 @@ import (
 )
 
 type handler struct {
-	HandlerFunc     func(w http.ResponseWriter, r *http.Request) (status int, err error)
-	CustomServeHTTP func(w http.ResponseWriter, r *http.Request, status int, err error)
+	handlerFunc     func(w http.ResponseWriter, r *http.Request) (status int, err error)
+	customServeHTTP func(w http.ResponseWriter, r *http.Request, status int, err error)
 }
 
 // ServeHTTP handles all the errors from the HTTP handlers.
 func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	status, err := fn.HandlerFunc(w, r)
-	fn.CustomServeHTTP(w, r, status, err)
+	status, err := fn.handlerFunc(w, r)
+	fn.customServeHTTP(w, r, status, err)
 }
--- app/lib/router/helper.go
+++ app/lib/router/helper.go
@@ -5,55 +5,55 @@ import "net/http"
 // Delete is a shortcut for router.Handle("DELETE", path, handle)
 func (m *Mux) Delete(path string, fn func(http.ResponseWriter, *http.Request) (int, error)) {
 	m.router.Handle("DELETE", path, handler{
-		HandlerFunc:     fn,
-		CustomServeHTTP: m.customServeHTTP,
+		handlerFunc:     fn,
+		customServeHTTP: m.customServeHTTP,
 	})
 }
 
 // Get is a shortcut for router.Handle("GET", path, handle)
 func (m *Mux) Get(path string, fn func(http.ResponseWriter, *http.Request) (int, error)) {
 	m.router.Handle("GET", path, handler{
-		HandlerFunc:     fn,
-		CustomServeHTTP: m.customServeHTTP,
+		handlerFunc:     fn,
+		customServeHTTP: m.customServeHTTP,
 	})
 }
 
 // Head is a shortcut for router.Handle("HEAD", path, handle)
 func (m *Mux) Head(path string, fn func(http.ResponseWriter, *http.Request) (int, error)) {
 	m.router.Handle("HEAD", path, handler{
-		HandlerFunc:     fn,
-		CustomServeHTTP: m.customServeHTTP,
+		handlerFunc:     fn,
+		customServeHTTP: m.customServeHTTP,
 	})
 }
 
 // Options is a shortcut for router.Handle("OPTIONS", path, handle)
 func (m *Mux) Options(path string, fn func(http.ResponseWriter, *http.Request) (int, error)) {
 	m.router.Handle("OPTIONS", path, handler{
-		HandlerFunc:     fn,
-		CustomServeHTTP: m.customServeHTTP,
+		handlerFunc:     fn,
+		customServeHTTP: m.customServeHTTP,
 	})
 }
 
 // Patch is a shortcut for router.Handle("PATCH", path, handle)
 func (m *Mux) Patch(path string, fn func(http.ResponseWriter, *http.Request) (int, error)) {
 	m.router.Handle("PATCH", path, handler{
-		HandlerFunc:     fn,
-		CustomServeHTTP: m.customServeHTTP,
+		handlerFunc:     fn,
+		customServeHTTP: m.customServeHTTP,
 	})
 }
 
 // Post is a shortcut for router.Handle("POST", path, handle)
 func (m *Mux) Post(path string, fn func(http.ResponseWriter, *http.Request) (int, error)) {
 	m.router.Handle("POST", path, handler{
-		HandlerFunc:     fn,
-		CustomServeHTTP: m.customServeHTTP,
+		handlerFunc:     fn,
+		customServeHTTP: m.customServeHTTP,
 	})
 }
 
 // Put is a shortcut for router.Handle("PUT", path, handle)
 func (m *Mux) Put(path string, fn func(http.ResponseWriter, *http.Request) (int, error)) {
 	m.router.Handle("PUT", path, handler{
-		HandlerFunc:     fn,
-		CustomServeHTTP: m.customServeHTTP,
+		handlerFunc:     fn,
+		customServeHTTP: m.customServeHTTP,
 	})
 }
