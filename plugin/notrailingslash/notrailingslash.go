aff3897fe6aa8c01383dab950de15711d1a0af43
Joseph Spurrier <github@josephspurrier.com>
2021-04-16 23:00:32 -0400

    Improve no trailing slash to support debug/pprof

--- plugin/notrailingslash/notrailingslash.go
+++ plugin/notrailingslash/notrailingslash.go
@@ -4,6 +4,7 @@ package notrailingslash
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/josephspurrier/ambient"
 )
@@ -42,4 +43,34 @@ func (p *Plugin) Middleware() []func(next http.Handler) http.Handler {
 	return []func(next http.Handler) http.Handler{
 		p.stripSlash,
 	}
+}
+
+// StripSlash will strip trailing slashes from requests.
+func (p *Plugin) stripSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Don't allow access to files with a slash at the end.
+		if strings.Contains(r.URL.Path, ".") && strings.HasSuffix(r.URL.Path, "/") {
+			p.Mux.Error(http.StatusNotFound, w, r)
+			return
+		}
+
+		// Allow access to debug/pprof and force a trailing slash.
+		if strings.HasPrefix(r.URL.Path, "/debug") {
+			if r.URL.Path == "/debug/pprof" {
+				http.Redirect(w, r, r.URL.Path+"/", http.StatusPermanentRedirect)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// Strip trailing slash.
+		if r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
+			http.Redirect(w, r, strings.TrimRight(r.URL.Path, "/"), http.StatusPermanentRedirect)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
