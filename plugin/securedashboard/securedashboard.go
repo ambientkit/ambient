5ed3b69158c76be8c88b0762716b87122cccbb8b
Joseph Spurrier <github@josephspurrier.com>
2021-04-16 22:46:20 -0400

    Simplify securedashboard

--- plugin/securedashboard/securedashboard.go
+++ plugin/securedashboard/securedashboard.go
@@ -4,6 +4,7 @@ package securedashboard
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/josephspurrier/ambient"
 )
@@ -49,4 +50,27 @@ func (p *Plugin) Middleware() []func(next http.Handler) http.Handler {
 	return []func(next http.Handler) http.Handler{
 		p.DisallowAnon,
 	}
+}
+
+// DisallowAnon does not allow anonymous users to access the page.
+func (p *Plugin) DisallowAnon(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Don't allow anon users to access the dashboard.
+		if strings.HasPrefix(r.URL.Path, "/dashboard") {
+			// If user is not authenticated, don't allow them to access the page.
+			loggedIn, err := p.Site.UserAuthenticated(r)
+			// If there was an error, then return error.
+			if err != nil {
+				status, _ := p.Site.Error(err)
+				p.Mux.Error(status, w, r)
+				return
+			}
+			if !loggedIn {
+				http.Redirect(w, r, "/", http.StatusFound)
+				return
+			}
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
