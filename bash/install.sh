cb10393068d4552334ece88e9706c064ef24d32b
Joseph Spurrier <github@josephspurrier.com>
2022-01-16 23:10:53 -0500

    Add install script instead of 'go install'

---
+++ bash/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build to support 'replace' in modules since `go install` isn't supported.
+# Issue: https://github.com/golang/go/issues/40276
+git clone --depth 1 https://github.com/josephspurrier/ambient.git
+chmod -R +w ambient
+cd ambient
+go build -o ../amb cmd/amb/main.go
+cd ..
+rm -r ambient
