2de3a3c95be2b381d4f23f342d2c42c592d62680
Joseph Spurrier <github@josephspurrier.com>
2022-01-30 17:20:09 -0500

    Add go to push

--- bash/update-dependencies.sh
+++ bash/update-dependencies.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
+# Exit when any command fails.
+set -e
+
 AMBIENTHASH=$(git rev-parse HEAD)
 
 cd ../plugin
-PLUGINHASH=$(git rev-parse HEAD)
+go get github.com/ambientkit/ambient@${AMBIENTHASH}
+go mod tidy -compat=1.17
 git add go.mod
 git add go.sum
-# git commit -m "Update dependencies"
-go get github.com/ambientkit/ambient@${AMBIENTHASH}}
-go mod tidy -compat=1.17
+go test ./...
+git commit -m "Update dependencies (automated)"
+git push
+PLUGINHASH=$(git rev-parse HEAD)
 
 cd ../ambient-template
+go get github.com/ambientkit/ambient@${AMBIENTHASH}
+go get github.com/ambientkit/plugin@${PLUGINHASH}
+go mod tidy -compat=1.17
 git add go.mod
 git add go.sum
-# git commit -m "Update dependencies"
-go get github.com/ambientkit/ambient@${AMBIENTHASH}}
-go mod tidy -compat=1.17
+go test ./...
+git commit -m "Update dependencies (automated)"
+git push
 
 cd ../amb
+go get github.com/ambientkit/ambient@${AMBIENTHASH}
+go get github.com/ambientkit/plugin@${PLUGINHASH}
+go mod tidy -compat=1.17
 git add go.mod
 git add go.sum
-# git commit -m "Update dependencies"
-go get github.com/ambientkit/ambient@${AMBIENTHASH}}
-go mod tidy -compat=1.17
+go test ./...
+git commit -m "Update dependencies (automated)"
