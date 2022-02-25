dd627d9e572feff60c5157f018141ef9fd8e9ac9
Joseph Spurrier <github@josephspurrier.com>
2022-02-24 20:47:22 -0500

    Fix the build errors on the template changes

--- safe_templateengine.go
+++ safe_templateengine.go
@@ -32,27 +32,44 @@ func globalFuncMap(fm template.FuncMap) template.FuncMap {
 	return fm
 }
 
+// globalFuncMapCallable returns a callable function.
+func globalFuncMapCallable(fm template.FuncMap) func(r *http.Request) template.FuncMap {
+	return func(r *http.Request) template.FuncMap {
+		return globalFuncMap(fm)
+	}
+}
+
 // Page renders a page.
-func (rr *TemplateRenderer) Page(w http.ResponseWriter, r *http.Request, assets embed.FS, templateName string, fm template.FuncMap, vars map[string]interface{}) (status int, err error) {
-	return rr.render.Page(w, r, assets, templateName, globalFuncMap(fm), vars)
+func (rr *TemplateRenderer) Page(w http.ResponseWriter, r *http.Request,
+	assets embed.FS, templateName string, fm func(r *http.Request) template.FuncMap,
+	vars map[string]interface{}) (status int, err error) {
+	return rr.render.Page(w, r, assets, templateName, globalFuncMapCallable(fm(r)), vars)
 }
 
 // PageContent renders page content.
-func (rr *TemplateRenderer) PageContent(w http.ResponseWriter, r *http.Request, content string, fm template.FuncMap, vars map[string]interface{}) (status int, err error) {
-	return rr.render.PageContent(w, r, content, globalFuncMap(fm), vars)
+func (rr *TemplateRenderer) PageContent(w http.ResponseWriter, r *http.Request,
+	content string, fm func(r *http.Request) template.FuncMap,
+	vars map[string]interface{}) (status int, err error) {
+	return rr.render.PageContent(w, r, content, globalFuncMapCallable(fm(r)), vars)
 }
 
 // Post renders a post.
-func (rr *TemplateRenderer) Post(w http.ResponseWriter, r *http.Request, assets embed.FS, templateName string, fm template.FuncMap, vars map[string]interface{}) (status int, err error) {
-	return rr.render.Post(w, r, assets, templateName, globalFuncMap(fm), vars)
+func (rr *TemplateRenderer) Post(w http.ResponseWriter, r *http.Request,
+	assets embed.FS, templateName string, fm func(r *http.Request) template.FuncMap,
+	vars map[string]interface{}) (status int, err error) {
+	return rr.render.Post(w, r, assets, templateName, globalFuncMapCallable(fm(r)), vars)
 }
 
 // PostContent renders post content.
-func (rr *TemplateRenderer) PostContent(w http.ResponseWriter, r *http.Request, content string, fm template.FuncMap, vars map[string]interface{}) (status int, err error) {
-	return rr.render.PostContent(w, r, content, globalFuncMap(fm), vars)
+func (rr *TemplateRenderer) PostContent(w http.ResponseWriter, r *http.Request,
+	content string, fm func(r *http.Request) template.FuncMap,
+	vars map[string]interface{}) (status int, err error) {
+	return rr.render.PostContent(w, r, content, globalFuncMapCallable(fm(r)), vars)
 }
 
 // Error renders an error.
-func (rr *TemplateRenderer) Error(w http.ResponseWriter, r *http.Request, content string, statusCode int, fm template.FuncMap, vars map[string]interface{}) (status int, err error) {
-	return rr.render.Error(w, r, content, statusCode, globalFuncMap(fm), vars)
+func (rr *TemplateRenderer) Error(w http.ResponseWriter, r *http.Request,
+	content string, statusCode int, fm func(r *http.Request) template.FuncMap,
+	vars map[string]interface{}) (status int, err error) {
+	return rr.render.Error(w, r, content, statusCode, globalFuncMapCallable(fm(r)), vars)
 }
