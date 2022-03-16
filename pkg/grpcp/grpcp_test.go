f157366bec406396ec4720cdc7b7ca7e338ce5ff
Joseph Spurrier <github@josephspurrier.com>
2022-03-16 19:41:53 -0400

    Consolidate test helper

--- pkg/grpcp/grpcp_test.go
+++ pkg/grpcp/grpcp_test.go
@@ -10,8 +10,13 @@ import (
 	"testing"
 
 	"github.com/ambientkit/ambient"
-	"github.com/ambientkit/ambient/internal/testutil"
 	"github.com/ambientkit/ambient/pkg/ambientapp"
+	"github.com/ambientkit/ambient/pkg/grpcp/testdata/plugin/neighbor"
+	"github.com/ambientkit/plugin/logger/zaplogger"
+	"github.com/ambientkit/plugin/router/awayrouter"
+	"github.com/ambientkit/plugin/sessionmanager/scssession"
+	"github.com/ambientkit/plugin/storage/memorystorage"
+	"github.com/ambientkit/plugin/templateengine/htmlengine"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,7 +30,7 @@ func grpcSetup(t *testing.T) *ambientapp.App {
 	}
 
 	// Set up the application.
-	app, err := testutil.Setup()
+	app, err := Setup()
 	if err != nil {
 		assert.FailNow(t, err.Error())
 	}
@@ -161,4 +166,52 @@ func TestMain(t *testing.T) {
 	resp, body = doRequest(t, mux, httptest.NewRequest("GET", "/neighborPluginGranted", nil))
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 	assert.Equal(t, "Granted: true", string(body))
+}
+
+// Setup sets up a test gRPC server.
+func Setup() (*ambientapp.App, error) {
+	h := func(log ambient.Logger, renderer ambient.Renderer, w http.ResponseWriter, r *http.Request, err error) {
+		if err != nil {
+			switch e := err.(type) {
+			case ambient.Error:
+				errText := e.Error()
+				if len(errText) == 0 {
+					errText = http.StatusText(e.Status())
+				}
+				http.Error(w, errText, e.Status())
+			default:
+				http.Error(w, http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}
+	}
+
+	sessPlugin := scssession.New("5ba3ad678ee1fd9c4fddcef0d45454904422479ed762b3b0ddc990e743cb65e0")
+	plugins := &ambient.PluginLoader{
+		// Core plugins are implicitly trusted.
+		Router:         awayrouter.New(h),
+		TemplateEngine: htmlengine.New(),
+		SessionManager: sessPlugin,
+		// Trusted plugins are those that are typically needed to boot so they
+		// will be enabled and given full access.
+		TrustedPlugins: map[string]bool{},
+		Plugins: []ambient.Plugin{
+			//hello.New(),
+			neighbor.New(),
+		},
+		GRPCPlugins: []ambient.GRPCPlugin{
+			{PluginName: "hello", PluginPath: "./pkg/grpcp/testdata/plugin/hello/cmd/plugin/hello"},
+		},
+		Middleware: []ambient.MiddlewarePlugin{
+			// Middleware - executes bottom to top.
+			sessPlugin,
+		},
+	}
+	app, _, err := ambientapp.NewApp("myapp", "1.0",
+		zaplogger.New(),
+		ambient.StoragePluginGroup{
+			Storage: memorystorage.New(),
+		},
+		plugins)
+	return app, err
 }
