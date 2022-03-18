8ba2f59ed2b5aaf4a052c30f04c447819f134490
Joseph Spurrier <github@josephspurrier.com>
2022-03-18 08:13:10 -0400

    Clean up conversion

--- pkg/grpcp/conversion.go
+++ pkg/grpcp/conversion.go
@@ -16,11 +16,13 @@ func ObjectToProtobufStruct(obj interface{}) (*structpb.Struct, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	s := &structpb.Struct{}
 	err = protojson.Unmarshal(b, s)
 	if err != nil {
 		return nil, err
 	}
+
 	return s, nil
 }
 
@@ -31,39 +33,38 @@ func ProtobufStructToObject(s *structpb.Struct, obj interface{}) error {
 	if v.Kind() != reflect.Ptr {
 		return errors.New("must pass a pointer, not a value")
 	}
+
 	b, err := protojson.Marshal(s)
 	if err != nil {
 		return err
 	}
+
 	return json.Unmarshal(b, &obj)
 }
 
 // ArrayToProtobufStruct converts an array to a protobuf struct.
 func ArrayToProtobufStruct(obj interface{}) ([]*structpb.Struct, error) {
-	switch reflect.TypeOf(obj).Kind() {
-	case reflect.Slice:
-		arr := reflect.ValueOf(obj)
-		s := make([]*structpb.Struct, arr.Len())
-		for i := 0; i < arr.Len(); i++ {
-			// typeOf := reflect.TypeOf(arr.Index(i))
-			// val := reflect.New(typeOf.Elem())
-			// arr.Index(i).Interface()
-			//b, err := json.Marshal(val.Interface())
-			b, err := json.Marshal(arr.Index(i).Interface())
-			if err != nil {
-				return nil, err
-			}
-			vv := &structpb.Struct{}
-			err = protojson.Unmarshal(b, vv)
-			if err != nil {
-				return nil, err
-			}
-			s[i] = vv
-		}
-		return s, nil
+	val := reflect.TypeOf(obj)
+	if val.Kind() != reflect.Slice {
+		return nil, fmt.Errorf("must pass a slice, not a: %v", val.Kind())
 	}
 
-	return nil, errors.New("must pass in an array")
+	arr := reflect.ValueOf(obj)
+	s := make([]*structpb.Struct, arr.Len())
+	for i := 0; i < arr.Len(); i++ {
+		b, err := json.Marshal(arr.Index(i).Interface())
+		if err != nil {
+			return nil, err
+		}
+		vv := &structpb.Struct{}
+		err = protojson.Unmarshal(b, vv)
+		if err != nil {
+			return nil, err
+		}
+		s[i] = vv
+	}
+
+	return s, nil
 }
 
 // ProtobufStructToArray converts a protobuf struct to an array.
@@ -72,11 +73,11 @@ func ProtobufStructToArray(s []*structpb.Struct, obj interface{}) error {
 	val := reflect.ValueOf(obj)
 	if val.Kind() != reflect.Ptr {
 		return errors.New("must pass a pointer, not a: value")
-	} else if reflect.TypeOf(obj).Elem().Kind() != reflect.Slice {
-		return fmt.Errorf("must pass a slice, not a: %v", reflect.TypeOf(obj).Elem().Kind())
+	} else if val.Elem().Kind() != reflect.Slice {
+		return fmt.Errorf("must pass a slice, not a: %v", val.Elem().Kind())
 	}
 
-	objVal := reflect.ValueOf(obj).Elem()
+	objVal := val.Elem()
 
 	for i, v := range s {
 		b, err := protojson.Marshal(v)
